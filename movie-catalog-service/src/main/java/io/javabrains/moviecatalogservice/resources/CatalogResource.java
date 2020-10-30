f2ec698ccd2d9882cedcec024ac5b75e84d24d4a
Fernando Samano <SamanoF21@gmail.com>
2020-10-29 19:28:57 -0700

    Changed calling from URL to calling from Eureka server

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -23,10 +23,10 @@ public class CatalogResource {
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalogue(@PathVariable String userId) {
-		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
+		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/"+userId, UserRating.class);
 
 		return ratings.getUserRating().stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 			return new CatalogItem(movie.getName(), "Description", rating.getRating());
 		}).collect(Collectors.toList());
 	}
